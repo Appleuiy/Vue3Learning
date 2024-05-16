1c3d4503e9b46c9b8d1c641d327548700bc405d7
appleuiy <2856206091@qq.com>
2024-05-16 17:19:14 +0800

    reactive

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="person">
-    <h2>姓名：{{ name }}</h2>
-    <h2>年龄：{{ age }}</h2>
-    <h2>地址：{{address }}</h2>
-    <button @click="changeName">名字变一变</button>
-    <button @click="changeAge">年龄+1</button>
-    <button @click="showTel">点我拿电话</button>
+    <h2>一辆车价值：{{car.brand }}车价值{{ car.price }} 万</h2>
+    <button @click="changePrice">修改价格</button>
   </div>
+  <br>
+  <h2>游戏列表：</h2>
+  <ul>
+    <li v-for="game in games" :key="game.id">{{game.name}}</li>
+    <button @click="changeFirstGameName">修改第一个游戏名字</button>
+  </ul>
 </template>
 
 <script lang="ts">
@@ -16,23 +18,22 @@
 </script>
 
 <script lang="ts" setup>  //语法糖
-    import {ref} from 'vue'  //ref创建响应式数据
-    let age = ref(22)
-    let name = ref("张飘")  // 加上ref， 修改时候要对value更改   
-    let tel = "15085462605" 
-    let address = "北京市海淀区知春路大运村学生公寓"
-    function changeAge() {
-      age.value += 1
-    }
-
-    function changeName() {
-      name.value = "夏思嘉必定考好"  
-      console.log(name) 
-    }
-
-    function showTel() {
-      alert(age)
-    }
+   import { reactive } from 'vue';  //修改对象
+   // 数据
+   let car = reactive({brand: '奔驰', price: 100})   // 被代理变为 Proxy
+   let games = reactive([
+    {id:"01", name:"LOL"},
+    {id:"02", name:"原神"},
+    {id:"03", name:"王者农药"}
+   ])
+   console.log(car)
+   function changePrice() {
+    car.price += 10
+   }
+   
+   function changeFirstGameName() {
+    games[0].name = "魂斗罗"
+   }
 </script>
 
 
@@ -42,5 +43,11 @@
     box-shadow: 0 0 10px;
     border-radius: 10px;
     padding: 20px;
+  }
+  button {
+    margin: 0 5px;
+  }
+  li {
+    font-size: 20px;
   }
 </style>
