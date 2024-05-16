4004ec12d376bf8a732deb1ec90742cccae9b42b
appleuiy <2856206091@qq.com>
2024-05-16 16:57:59 +0800

    ref创建响应式数据

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -2,6 +2,7 @@
   <div class="person">
     <h2>姓名：{{ name }}</h2>
     <h2>年龄：{{ age }}</h2>
+    <h2>地址：{{address }}</h2>
     <button @click="changeName">名字变一变</button>
     <button @click="changeAge">年龄+1</button>
     <button @click="showTel">点我拿电话</button>
@@ -15,16 +16,17 @@
 </script>
 
 <script lang="ts" setup>  //语法糖
-    let age = "22"
-    let name = "张飘"
+    import {ref} from 'vue'  //ref创建响应式数据
+    let age = ref(22)
+    let name = ref("张飘")  // 加上ref， 修改时候要对value更改   
     let tel = "15085462605" 
-
+    let address = "北京市海淀区知春路大运村学生公寓"
     function changeAge() {
-      age += 1
+      age.value += 1
     }
 
     function changeName() {
-      name = "夏思嘉必定考好"  
+      name.value = "夏思嘉必定考好"  
       console.log(name) 
     }
 
