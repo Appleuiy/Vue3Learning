8b5c7639a13366fd0984d1d54f23f605aa37faac
appleuiy <2856206091@qq.com>
2024-05-16 16:51:42 +0800

    setup语法糖

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -2,41 +2,43 @@
   <div class="person">
     <h2>姓名：{{ name }}</h2>
     <h2>年龄：{{ age }}</h2>
-    <button @click="changeName">修改名字</button>
+    <button @click="changeName">名字变一变</button>
     <button @click="changeAge">年龄+1</button>
-    <button @click="showTel">点我查看联系方式</button>
+    <button @click="showTel">点我拿电话</button>
   </div>
 </template>
 
 <script lang="ts">
-export default {
-  name: "App",
-  data() {
-    return {
-      name: "张三",
-      age: 18,
-      tel: "13888888888",
-    };
-  },
-  methods: {
-    changeName() {
-      this.name = "zhang-san";
-    },
-    changeAge() {
-      this.age += 1;
-    },
-    showTel() {
-      alert(this.tel);
-    },
-  },
-};
+    export default {
+      name: 'Person',
+    }
 </script>
 
+<script lang="ts" setup>  //语法糖
+    let age = "22"
+    let name = "张飘"
+    let tel = "15085462605" 
+
+    function changeAge() {
+      age += 1
+    }
+
+    function changeName() {
+      name = "夏思嘉必定考好"  
+      console.log(name) 
+    }
+
+    function showTel() {
+      alert(age)
+    }
+</script>
+
+
 <style>
-.person {
-  background-color: skyblue;
-  box-shadow: 0 0 10px;
-  border-radius: 10px;
-  padding: 20px;
-}
+  .person {
+    background-color: skyblue;
+    box-shadow: 0 0 10px;
+    border-radius: 10px;
+    padding: 20px;
+  }
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,13 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import VueSetupExtend from 'vite-plugin-vue-setup-extend' // 引入我下载的扩展
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    VueSetupExtend()   // 自己下载的扩展
   ],
   resolve: {
     alias: {
